a7ac7b5ea1d9ef70c1a7cade4e53b987d6a9cf35
naivsheng <naivsheng@outlook.com>
2021-12-02 18:58:48 +0100

    get token from DB and active

--- active.php
+++ active.php
@@ -6,14 +6,29 @@
     // 创建连接
     $conn = mysqli_connect("localhost","root","admin",'db');
     // check
-    $query = sprintf("SELECT * FROM tmp WHERE `id`=%s and `verify_code`=%s;",$id,$verify_code);
+    //$query = sprintf("SELECT * FROM test WHERE `id`=%s and `verify_code`=%s;",$id,$verify_code);
+    $query = "SELECT * FROM test WHERE `id`=".$id;
     $result = mysqli_query($conn,$query);
     if ($result){
         // update
-        $query = sprintf("UPDATE tmp SET verify_code='-' WHERE id=%s;",$id);
+        /*
+        $query = sprintf("UPDATE test SET verify_code='-' WHERE id=%s;",$id);
         mysqli_query($conn,$query);
         echo 'verify erfolgt';
-    }
+        */
+        ///*
+        while($row = mysqli_fetch_array($result))
+        {
+            //echo $row['id'] . " " . $row['verify_code'];
+            //echo "<br>";
+            if($row['verify_code'] == $verify_code){
+                $query = sprintf("UPDATE test SET verify_code='-' WHERE id=%s;",$id);
+                mysqli_query($conn,$query);
+                echo 'verify erfolgt';
+            }
+            }
+        //*/
+        }
     else {
         echo 'Falsch Code!';
     }
