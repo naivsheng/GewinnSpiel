fb7c588b394c979b8badb052492c348d0b914564
naivsheng <naivsheng@outlook.com>
2021-12-03 10:00:10 +0100

    跳过已处理过的记录，发送邮件时生成token

--- active.php
+++ active.php
@@ -6,28 +6,18 @@
     // 创建连接
     $conn = mysqli_connect("localhost","root","admin",'db');
     // check
-    //$query = sprintf("SELECT * FROM test WHERE `id`=%s and `verify_code`=%s;",$id,$verify_code);
     $query = "SELECT * FROM test WHERE `id`=".$id;
     $result = mysqli_query($conn,$query);
     if ($result){
         // update
-        /*
-        $query = sprintf("UPDATE test SET verify_code='-' WHERE id=%s;",$id);
-        mysqli_query($conn,$query);
-        echo 'verify erfolgt';
-        */
-        ///*
         while($row = mysqli_fetch_array($result))
         {
-            //echo $row['id'] . " " . $row['verify_code'];
-            //echo "<br>";
             if($row['verify_code'] == $verify_code){
                 $query = sprintf("UPDATE test SET verify_code='-' WHERE id=%s;",$id);
                 mysqli_query($conn,$query);
                 echo 'verify erfolgt';
             }
             }
-        //*/
         }
     else {
         echo 'Falsch Code!';
--- register.php
+++ register.php
@@ -1,5 +1,5 @@
 <?php
-// 发送邮件
+// 发送邮件时生成token
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
@@ -7,7 +7,21 @@ require './src/Exception.php';
 require './src/PHPMailer.php';
 require './src/SMTP.php';
 
-$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+$conn = mysqli_connect("localhost","root","admin",'db');
+$query = "SELECT * FROM test WHERE verify_code is Null;";
+$result = mysqli_query($conn,$query);
+while($row = mysqli_fetch_array($result))
+{
+    $str=rand();
+    $token = md5($str);
+    if(sendMail($row['id'],$row['email'],$token)){
+        $query = "UPDATE test SET verify_code = ".$token." WHERE `id` = ".$row['id'].";";
+        mysqli_query($conn,$query);
+    }
+}
+function sendMail($id,$email,$token)
+{
+  $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
 try {
     //服务器配置
     $mail->CharSet ="UTF-8";                     //设定邮件编码
@@ -20,14 +34,15 @@ try {
     $mail->SMTPSecure = 'ssl';                    // 允许 TLS 或者ssl协议
     $mail->Port = 465;                            // 服务器端口 25 或者465 具体要看邮箱服务器支持
 
-    $mail->setFrom("", 'Mailer');  //发件人
-    $mail->addAddress('', 'Jue');  // 收件人
-    $mail->addReplyTo('', 'info'); //回复的时候回复给哪个邮箱 建议和发件人一致
+    $mail->setFrom("", 'No Reply');  //发件人
+    $mail->addAddress($email, 'GA Kunde');  // 收件人
+    $mail->addReplyTo('', 'No Reply'); //回复的时候回复给哪个邮箱 建议和发件人一致
+    
     //Content
     $mail->isHTML(true);                                  // 是否以HTML文档格式发送  发送后客户端可直接显示对应HTML内容
-    $token = '12345';
-    $id = '1';
-    $mail->Subject = 'Title' . time();
+    $token = $token;
+    $id = $id;
+    $mail->Subject = 'confirm your email-adress' . time();
     $mail->Body    = "Sehr geehrte Damen und Herren,<br>
      <br>
     Sie haben schon ihre Document hochgeladen.<br>
@@ -35,7 +50,8 @@ try {
      <br>
     <a href='http://localhost:3000/active.php?id=".$id."&verify_code=".$token."'>klicken</a></br>
     <br>
-    Wenn der obige Link nicht angeklickt werden kann, kopieren Sie ihn bitte in die Adressleiste Ihres Browsers, um darauf zuzugreifen. <br>
+    Wenn der obige Link nicht angeklickt werden kann, kopieren Sie diesen Link bitte in die Adressleiste Ihres Browsers, um darauf zuzugreifen. <br>
+    http://localhost:3000/active.php?id=".$id."&verify_code=".$token." <br>
    Falls diese Aktivierungsanfrage nicht von Ihnen gesendet wurde, ignorieren Sie diese E-Mail bitte. <br>
    <br>
     Mit freundlichen Grüßen.<br>
@@ -44,10 +60,11 @@ try {
     亲爱的顾客您好：<br>
   感谢您在我站上传了抽奖证明。<br>
   请点击链接激活您的本次上传。<br>
-   <br>
-    <a href='http://localhost:3000/active.php?id=".$id."&verify_code=".$token."'>点击此处</a></br>
+  <br>
+  <a href='http://localhost:3000/active.php?id=".$id."&verify_code=".$token."'>klicken</a></br>
     <br>
-    如果以上链接无法点击，请将它复制到你的浏览器地址栏中进入访问。<br>
+  如果以上链接无法点击，请将以下链接复制到你的浏览器地址栏中进入访问。<br>
+  http://localhost:3000/active.php?id=".$id."&verify_code=".$token." <br>
   如果此次激活请求非你本人所发，请忽略本邮件。<br>
   <br>
   祝您生活愉快<br>
@@ -59,6 +76,10 @@ try {
     Bitte loggen Sie sich in Ihr Postfach ein, um Ihre Upload-Ergebnisse rechtzeitig zu aktivieren.<br>
     恭喜您，上传成功！
 请登录到您的邮箱及时激活您的上传结果！';
+return True;
 } catch (Exception $e) {
     echo '邮件发送失败: ', $mail->ErrorInfo;
+    echo '<br>Fehler bei Emailsendung!<br>';
+    return False;
+}
 }
